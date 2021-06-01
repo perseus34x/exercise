fa804b10c4593726819a60a5d92115bb66a8ff4a
benliu <benliu@benlius-MacBook-Pro.local>
2021-06-01 20:45:27 +0800

    exercise code for rust

---
+++ rust_exercise/hello_world/target/debug/deps/hello_world.d
@@ -0,0 +1,5 @@
+/Users/benliu/work/misc/rust_exercise/hello_world/target/debug/deps/hello_world: src/main.rs
+
+/Users/benliu/work/misc/rust_exercise/hello_world/target/debug/deps/hello_world.d: src/main.rs
+
+src/main.rs:
