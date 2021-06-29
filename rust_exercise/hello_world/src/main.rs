66f8c8794aa6b28af637cbf61fad10d8c684f7f1
Luxebeng <luxebeng@gmail.com>
2021-06-29 17:35:02 +0800

    exercise code for rust

--- rust_exercise/hello_world/src/main.rs
+++ rust_exercise/hello_world/src/main.rs
@@ -28,4 +28,67 @@ fn main() {
     println!("Pi is roughly {1:.0$}",  3, pi);
     println!("Pi is roughly {:.*}",  4, pi);
 
+
+    #[derive(Debug)]
+    struct Structure1(i32);
+    #[derive(Debug)]
+    struct Deep(Structure1);
+    println!("Now {:?} will print.", Structure1(3));
+    println!("Now {:?} will pirnt.", Deep(Structure1(7)));
+
+    #[derive(Debug)]
+    struct Person<'a> {
+        name : &'a str,
+        age  : u8
+    }
+    let name = "Peter";
+    let age = 18;
+    let peter = Person{name, age};
+    println!("{:#?}", peter);
+
+
+    use std::fmt;
+    struct Structure2(i32);
+    impl fmt::Display for Structure2 {
+        fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+            write!(f, "{}", self.0)
+        }
+    }
+
+    #[derive(Debug)]
+    struct MinMax(i64, i64);
+    impl fmt::Display for MinMax {
+        fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+            write!(f, "({}, {})", self.0, self.1)
+        }
+    }
+
+    #[derive(Debug)]
+    struct Point2D {
+        x: f64,
+        y: f64,
+    }
+    impl fmt::Display for Point2D {
+        fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+            write!(f, "x: {}, y: {}", self.x, self.y)
+        }
+    }
+
+    let minmax = MinMax(0, 14);
+    println!("Display {}", minmax);
+    println!("Debug: {:?}", minmax);
+
+    let big_range = MinMax(-300, 300);
+    let small_range = MinMax(-3, 3);
+    println!("The big range is {big} and the small is {small}",
+             small = small_range,
+             big = big_range);
+
+    let point = Point2D {x: 3.3, y: 7.2};
+    println!("compare points:");
+    println!("Display: {}", point);
+    println!("Debug: {:?}", point);
+    println!("Display: {} + {}i", point.x, point.y);
+    println!("Debug: Complex {{real: {real}, imag: {image} }}", real=point.x, image=point.y);
+
 }
