fe10c775ebda6f7c889b21710498106dc10e774a
luxebeng <luxebeng@gmail.com>
2022-08-09 23:56:07 +0800

    add chaper10

---
+++ rust_exercise/chapter10/lifetime_example/src/main.rs
@@ -0,0 +1,37 @@
+
+fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
+    if x.len() > y.len() {
+        x
+    } else {
+        y
+    }
+}
+
+fn first_word<'a>(s: &'a str) -> &'a str {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[0..i];
+        }
+    }
+
+    &s[..]
+}
+
+fn main() {
+    println!("Hello, world!");
+    println!("{}", first_word("Hello world!"));
+    let &mut r;
+    {
+        let x = 5;
+        r = &x;
+        println!("r: {}", r);
+    }
+
+    let string1 = String::from("abcd");
+    let string2 = "xyz";
+
+    let result = longest(string1.as_str(), string2);
+    println!("The longest string is {}", result);
+}
