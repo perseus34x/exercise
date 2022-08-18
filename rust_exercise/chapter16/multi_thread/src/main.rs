047560563acc7f85e397daf3b68f236555a868d6
luxebeng <luxebeng@gmail.com>
2022-08-18 17:42:05 +0800

    add chaper16

---
+++ rust_exercise/chapter16/multi_thread/src/main.rs
@@ -0,0 +1,54 @@
+use std::thread;
+use std::time::Duration;
+use std::sync::mpsc;
+
+fn main() {
+
+    let v = vec![1, 2, 3];
+
+    let handle = thread::spawn(move || {
+        println!("Here's a vector: {:?}", v);
+        for i in 1..10 {
+            println!("hi number {} from the spawned thread!", i);
+            thread::sleep(Duration::from_millis(1));
+        }
+    });
+    for j in 1..5 {
+        println!("hi number {} from the main thread!", j);
+        thread::sleep(Duration::from_millis(1));
+    }
+    println!("Hello, world!");
+    handle.join().unwrap();
+
+    let (tx, rx) = mpsc::channel();
+    let tx1 = tx.clone();
+    thread::spawn(move || {
+        let vals = vec![
+            String::from("hi"),
+            String::from("from"),
+            String::from("the"),
+            String::from("thread"),
+        ];
+
+        for val in vals {
+            tx.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+    thread::spawn(move || {
+        let vals = vec![
+            String::from("more"),
+            String::from("message"),
+            String::from("for"),
+            String::from("you"),
+        ];
+
+        for val in vals {
+            tx1.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+    for received in rx {
+        println!("Got: {}", received);
+    }
+}
