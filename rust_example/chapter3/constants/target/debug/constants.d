e062d6ca7dd927e0bc96768bccd495c469cedb71
luxebeng <luxebeng@gmail.com>
2022-09-02 11:07:37 +0800

    refector the code

--- rust_example/chapter3/constants/target/debug/constants.d
+++
@@ -1,1 +0,0 @@
-/Users/benliu/work/misc/rust_example/chapter3/constants/target/debug/constants: /Users/benliu/work/misc/rust_example/chapter3/constants/src/main.rs
--- rust_example/chapter3/linked_list/target/debug/linked_list.d
+++
@@ -1,1 +0,0 @@
-/Users/benliu/work/misc/rust_example/chapter3/linked_list/target/debug/linked_list: /Users/benliu/work/misc/rust_example/chapter3/linked_list/src/main.rs
