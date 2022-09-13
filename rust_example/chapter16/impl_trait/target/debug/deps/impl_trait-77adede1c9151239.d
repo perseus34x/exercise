f0a622ce0ad24e6dd30997687589ad0c1227c097
luxebeng <luxebeng@gmail.com>
2022-09-13 11:30:30 +0800

    add code for chapter16

---
+++ rust_example/chapter16/impl_trait/target/debug/deps/impl_trait-77adede1c9151239.d
@@ -0,0 +1,5 @@
+/Users/benliu/work/exercise/rust_example/chapter16/impl_trait/target/debug/deps/impl_trait-77adede1c9151239: src/main.rs
+
+/Users/benliu/work/exercise/rust_example/chapter16/impl_trait/target/debug/deps/impl_trait-77adede1c9151239.d: src/main.rs
+
+src/main.rs:
---
+++ rust_example/chapter16/impl_trait/target/debug/impl_trait.d
@@ -0,0 +1,1 @@
+/Users/benliu/work/exercise/rust_example/chapter16/impl_trait/target/debug/impl_trait: /Users/benliu/work/exercise/rust_example/chapter16/impl_trait/src/main.rs
