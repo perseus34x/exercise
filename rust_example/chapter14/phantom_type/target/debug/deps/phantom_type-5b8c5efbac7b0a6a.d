2630d63869289f4798ede5c8da8c17e47a6a3b19
luxebeng <luxebeng@gmail.com>
2022-09-14 08:38:27 +0800

    add code for chapter14

---
+++ rust_example/chapter14/phantom_type/target/debug/deps/phantom_type-5b8c5efbac7b0a6a.d
@@ -0,0 +1,5 @@
+/Users/benliu/work/exercise/rust_example/chapter14/phantom_type/target/debug/deps/phantom_type-5b8c5efbac7b0a6a: src/main.rs
+
+/Users/benliu/work/exercise/rust_example/chapter14/phantom_type/target/debug/deps/phantom_type-5b8c5efbac7b0a6a.d: src/main.rs
+
+src/main.rs:
---
+++ rust_example/chapter14/phantom_type/target/debug/phantom_type.d
@@ -0,0 +1,1 @@
+/Users/benliu/work/exercise/rust_example/chapter14/phantom_type/target/debug/phantom_type: /Users/benliu/work/exercise/rust_example/chapter14/phantom_type/src/main.rs
