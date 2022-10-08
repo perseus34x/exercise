1456b798df4ade6e58bcd4aafbbceaed54feb2e8
luxebeng <luxebeng@gmail.com>
2022-10-08 14:06:50 +0800

    add code for chapter2

--- rust_design_patterns/chapter2/ch2_1/target/debug/ch2_1.d
+++
@@ -1,1 +0,0 @@
-/Users/benliu/work/exercise/rust_design_patterns/ch2_1/target/debug/ch2_1: /Users/benliu/work/exercise/rust_design_patterns/ch2_1/src/main.rs
--- rust_design_patterns/chapter2/ch2_1/target/debug/deps/ch2_1-28dae399e28ea866.d
+++
@@ -1,5 +0,0 @@
-/Users/benliu/work/exercise/rust_design_patterns/ch2_1/target/debug/deps/ch2_1-28dae399e28ea866: src/main.rs
-
-/Users/benliu/work/exercise/rust_design_patterns/ch2_1/target/debug/deps/ch2_1-28dae399e28ea866.d: src/main.rs
-
-src/main.rs:
